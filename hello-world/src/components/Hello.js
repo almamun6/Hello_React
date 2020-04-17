f2b1f3f1db0a4b2b7acafbdcc39730e32966ed3e
Shiham <shiham626@gmail.com>
2020-04-17 16:17:54 +0600

    JSX

---
+++ hello-world/src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hello = () => {
+    return (
+        <div className="dummy-class">
+            <h1>
+                Hello JSX
+            </h1>
+        </div>
+    )
+}
+
+export default Hello
