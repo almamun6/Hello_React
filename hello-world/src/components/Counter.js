a1068627356cbfcd1e41c54e4f0ac6980cd5dba5
Shiham <shiham626@gmail.com>
2020-04-17 21:08:09 +0600

    setState (extended)

--- hello-world/src/components/Counter.js
+++ hello-world/src/components/Counter.js
@@ -11,27 +11,36 @@ class Counter extends Component {
     }
 
     increment() {
-        //this.state.count = this.state.count + 1
-        //"this.state" can be only assigned in constructor, otherwise it won't work
-        //so we use "this.setState"
+        // this.setState({
+        //     count: this.state.count + 1
+        // }, () => {
+        //     console.log('Callback value', this.state.count)
+        // })
+        // console.log(this.state.count)
 
-        this.setState({
-            count: this.state.count + 1
-        }, () => {
-            console.log('Callback value', this.state.count)
-        })
-        console.log(this.state.count)
-        /*calls to "setState" are asynchronous so that, this "console.log" is being called before the state
-        is actually set. you will find that the console value is 1 less then the rendered value
-        
-        so we used callback function*/
+        /*React may group multiple setState calls into a single update for better performance
+        you can check with the commented code above
+
+        so we use the code below
+        */
+        this.setState(prevState => ({
+            count: prevState.count + 1
+        }))
+    }
+
+    incrementFive(){
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
     }
 
     render() {
         return (
             <div>
                 <div>count - {this.state.count}</div>
-                <button onClick={() => this.increment()}>Increment</button>
+                <button onClick={() => this.incrementFive()}>Increment</button>
             </div>
         )
     }
