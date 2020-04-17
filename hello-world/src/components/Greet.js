1bd42a8fe2237fdaa3a417a8a39aab1661c4dc2a
Shiham <shiham626@gmail.com>
2020-04-17 12:04:35 +0600

    Functional Components "fat arrow syntax"

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-function Greet () {
-    return <h1>Hello World</h1>
-}
+// function Greet () {
+//     return <h1>Hello World</h1>
+// }
+
+//it is recommended to use ES6 fat arrow function, though commented code above will works the same
+const Greet = ()=> <h1>Hello World</h1>
 
 export default Greet
