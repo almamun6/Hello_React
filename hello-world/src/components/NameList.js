62a57c71cd36e03f5a49f0b0861f61daaab33518
Shiham <shiham626@gmail.com>
2020-04-18 14:41:27 +0600

    Lists and Keys

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -24,19 +24,22 @@ function NameList() {
 
     const persons = [
         {
+            id: 1,
             name: 'React',
             age: 7
         },
         {
+            id: 2,
             name: 'Angular',
             age: 10
         },
         {
+            id: 3,
             name: 'Node',
             age: 9
         }
     ]
-    const personList = persons.map(persn => <Person persn={persn} />)
+    const personList = persons.map(persn => <Person key={persn.id} persn={persn} />)
     return <div>{personList}</div>
 }
 
