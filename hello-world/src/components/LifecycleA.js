a60f29d053648dbf02842505d15f08f20c16ff4b
Shiham <shiham626@gmail.com>
2020-04-18 19:58:49 +0600

    Component Mounting Lifecycle Methods

---
+++ hello-world/src/components/LifecycleA.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class LifecycleA extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: 'React'
+        }
+        console.log('LifecycleA constructor')
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('LifecycleA getDerivedStateFromProps')
+        return null
+    }
+    componentDidMount() {
+        console.log('LifecycleA componentDidMount')
+    }
+    render() {
+        console.log('LifecycleA render')
+        return (
+            <div>
+                <div>
+                    Lifecycle A
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default LifecycleA
