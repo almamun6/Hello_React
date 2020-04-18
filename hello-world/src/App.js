7714c09688189c2e3a98764140ae237990a98045
Shiham <shiham626@gmail.com>
2020-04-18 19:21:54 +0600

    Basics of Form Handling

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,52 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick'
-import ClassClick from './components/ClassClick'
-import EventBind from './components/EventBind'
-import ParentComponent from './components/ParentComponent'
-import UserGreeting from './components/UserGreeting'
-import NameList from './components/NameList'
-import Stylesheet from './components/Stylesheet'
-import Inline from './components/Inline'
-import './components/appStyles.css'
-import styles from './components/appStyles.module.css'
-
+import Form from './components/Form'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Stylesheet />
-        <Inline />
-
-        <h1 className='error'>Error</h1>
-        <h1 className={styles.success}>Success</h1>
-        {/* <NameList /> */}
-        {/* <UserGreeting /> */}
-        {/* <ParentComponent /> */}
-        {/* <EventBind/> */}
-        {/* <FunctionClick />
-        <ClassClick /> */}
-        {/* <Counter/> */}
-        {/* <Message /> */}
-        {/* <Greet name="foo" age="25">
-          <p>This is children property</p>
-        </Greet>
-        <Greet name="moo" age="28">
-          <button>Action</button>
-        </Greet>
-        <Greet name="too" age="27" />
-
-        <Welcome name="foo" age="25" />
-        <Welcome name="moo" age="28" />
-        <Welcome name="too" age="27" /> */}
-        {/* <Hello /> */}
+        <Form />
       </div>
     )
   }
