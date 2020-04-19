ba5d3b9ebde8454c4659d1201e8b957179ecab88
Shiham <shiham626@gmail.com>
2020-04-19 11:16:49 +0600

    Forwarding Refs

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
+import FRPatentInput from './components/FRPatentInput';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FocusInput />
-        {/* <RefsDemo /> */}
+        <FRPatentInput />
       </div>
     )
   }
