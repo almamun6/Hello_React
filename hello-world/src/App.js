4a875e5d98ff537777d3e6048c6d49cd10ae5d44
Shiham <shiham626@gmail.com>
2020-04-19 16:22:43 +0600

    Render Props

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
+import User from './components/User';
+import ClickCounterTwo from './components/ClickCounterTwo';
+import HoverCounterTwo from './components/HoverCounterTwo';
+import Counters from './components/Counters';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ClickCounter />
-        <HoverCounter />
+        <User render={(isLoggedIn) => isLoggedIn ? 'React' : 'Angular'} />
+
+        <Counters>
+          {(count, incrementCount) => (
+            <ClickCounterTwo count={count} incrementCount={incrementCount} />
+          )}
+        </Counters>
+
+        <Counters>
+          {(count, incrementCount) => (
+            <HoverCounterTwo count={count} incrementCount={incrementCount} />
+          )}
+        </Counters>
+
       </div>
     )
   }
