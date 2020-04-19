a4e5ef247861006e4b795e98b46ffa13455ac5d2
Shiham <shiham626@gmail.com>
2020-04-19 18:16:25 +0600

    Context

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import User from './components/User';
-import ClickCounterTwo from './components/ClickCounterTwo';
-import HoverCounterTwo from './components/HoverCounterTwo';
-import Counters from './components/Counters';
+import ComponentC from './components/ComponentC';
+import { UserProvider } from './components/userContext';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <User render={(isLoggedIn) => isLoggedIn ? 'React' : 'Angular'} />
-
-        <Counters>
-          {(count, incrementCount) => (
-            <ClickCounterTwo count={count} incrementCount={incrementCount} />
-          )}
-        </Counters>
-
-        <Counters>
-          {(count, incrementCount) => (
-            <HoverCounterTwo count={count} incrementCount={incrementCount} />
-          )}
-        </Counters>
-
+        <UserProvider value="React">
+          <ComponentC />
+        </UserProvider>
       </div>
     )
   }
