ffdba9eecfba81004241b213eb89a9e535813a20
Shiham <shiham626@gmail.com>
2020-04-19 10:27:20 +0600

    Refs

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import PureComp from './components/PureComp'
-import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ParentComp />
+        <RefsDemo />
       </div>
     )
   }
